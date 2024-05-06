f7e04887363e5dab97739745e33e69518ae8871b
Pedro Muñoz <pemunoz2021@udec.cl>
2024-05-06 13:12:57 -0400

    aa

---
+++ Postgrado/run.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, url_for, redirect, jsonify, request, session
+import psycopg2
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime, timedelta
+
+conexion = psycopg2.connect(
+    host = "localhost",
+    database = "pruebas",
+    user = "admin",
+    password = "P.Q-37sop"
+)
+
+app = Flask(__name__)
+app.secret_key = 'tu_clave_secreta'
+
+usuario = None
+
+# Crear usuario
+
+#def create_user(username, password):
+    #hashed_password = generate_password_hash(password)
+    #cursor = conexion.cursor()
+    #cursor.execute('INSERT INTO login (usuario, contraseña) VALUES (%s, %s)', (username, hashed_password))
+    #conexion.commit()
+
+# Crear reserva
+def insert_reservation(usuario, puesto, fecha_agendamiento):
+    print(usuario, puesto, fecha_agendamiento)
+    # Verifica que todos los datos necesarios estén presentes
+    if not usuario or not puesto or not fecha_agendamiento:
+        print('Faltan datos para la reserva')
+        return
+
+    # Crea la consulta SQL
+    query = 'INSERT INTO RESERVAS (usuario, puesto, fecha_agendamiento) VALUES (%s, %s, %s)'
+
+    # Ejecuta la consulta
+    cursor = conexion.cursor()
+    cursor.execute(query, (usuario, puesto, fecha_agendamiento))
+    conexion.commit()
+    print('La consulta SQL se ejecutó correctamente')
+
+#insert_reservation('Pedroo', 10, '2024-04-30')
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        usuario = username
+        password = request.form['password']
+        
+        cursor = conexion.cursor()
+        cursor.execute('SELECT contraseña FROM login WHERE usuario = %s', (username,))
+        hashed_password = cursor.fetchone()
+        
+        if hashed_password is None or not check_password_hash(hashed_password[0], password):
+            error = 'Invalid username or password'
+        else:
+            session['username'] = username
+            return redirect(url_for('reserva'))
+        
+    return render_template("login.html", error=error)
+
+@app.route('/reservations/<date>', methods=['GET'])
+def get_reservations(date):
+    cursor = conexion.cursor()
+    cursor.execute('SELECT puesto FROM reservas WHERE fecha_agendamiento = %s', (date,))
+    reservations = [row[0] for row in cursor.fetchall()]
+    return jsonify(reservations)
+
+@app.route('/reservations', methods=['POST'])
+def create_reservation():
+
+    if 'username' in session:
+        usuario = session['username']
+        print(usuario)
+        data = request.get_json()
+        print(data)
+        puesto = data.get('puesto')
+        fecha_agendamiento = data.get('fecha_agendamiento')
+        insert_reservation(usuario, puesto, fecha_agendamiento)
+        return jsonify({'message': 'Reserva creada correctamente'}), 201
+    else:
+        return jsonify({'error': 'No se ha iniciado sesión'}), 401
+
+
+@app.route("/reserva")
+def reserva():
+    if 'username' in session:
+        return render_template("reserva.html", usuario=session['username'])
+
+    else:
+        return redirect(url_for('login'))
+
+def pagina_no_encontrada(error):
+    return redirect(url_for('login'))
+
+app.register_error_handler(404, pagina_no_encontrada)
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ Postgrado/static/js/script.js
@@ -0,0 +1,162 @@
+const container = document.querySelector(".container");
+const asientos = document.querySelectorAll(".container .asiento");
+const diaSeleccionado = document.getElementById("dia");
+var idAsientoSeleccionado = null;
+
+
+// Obtiene la fecha actual
+let fechaActual = new Date();
+
+// Obtiene los elementos de las opciones del menú desplegable
+const lunes = document.getElementById("lunes");
+const martes = document.getElementById("martes");
+const miercoles = document.getElementById("miercoles");
+const jueves = document.getElementById("jueves");
+const viernes = document.getElementById("viernes");
+
+// Calcula las fechas de los próximos días
+let fechaLunes = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), fechaActual.getDate() + (1 - fechaActual.getDay() + 7) % 7);
+let fechaMartes = new Date(fechaLunes.getFullYear(), fechaLunes.getMonth(), fechaLunes.getDate() + 1);
+let fechaMiercoles = new Date(fechaMartes.getFullYear(), fechaMartes.getMonth(), fechaMartes.getDate() + 1);
+let fechaJueves = new Date(fechaMiercoles.getFullYear(), fechaMiercoles.getMonth(), fechaMiercoles.getDate() + 1);
+let fechaViernes = new Date(fechaJueves.getFullYear(), fechaJueves.getMonth(), fechaJueves.getDate() + 1);
+
+// Actualiza las opciones del menú desplegable con las fechas
+lunes.textContent = "Lunes " + fechaLunes.toLocaleDateString();
+martes.textContent = "Martes " + fechaMartes.toLocaleDateString();
+miercoles.textContent = "Miércoles " + fechaMiercoles.toLocaleDateString();
+jueves.textContent = "Jueves " + fechaJueves.toLocaleDateString();
+viernes.textContent = "Viernes " + fechaViernes.toLocaleDateString();
+
+// Itera sobre cada asiento
+asientos.forEach((asiento, index) => {
+  // Crea un elemento span para contener el número del asiento
+  let numeroAsiento = document.createElement('span');
+  numeroAsiento.className = 'numero-asiento';
+
+  // Asigna el número del asiento (index + 1 porque los arrays empiezan en 0)
+  numeroAsiento.textContent = asientos.length - index;
+
+  // Añade el número del asiento al asiento
+  asiento.appendChild(numeroAsiento);
+});
+
+
+// Dia actual. Pasarlo a la base de datos al momento de reservar. Debe actualizarse.
+let diaActual = diaSeleccionado.value;
+let fechaSeleccionada = new Date(diaActual);
+
+// Evento de cambio en la selección de día
+diaSeleccionado.addEventListener("change", (e) => {
+
+  const asientosSeleccionados = document.querySelectorAll(".row .asiento.reservado");
+  asientosSeleccionados.forEach((asiento) => {
+    asiento.classList.remove("reservado");
+  });
+  idAsientoSeleccionado = null;
+
+  diaActual = e.target.value;
+
+  const partesFecha = diaActual.split(' ')[1].split('/');
+  fechaSeleccionada = `${partesFecha[2]}-${partesFecha[1]}-${partesFecha[0]}`; // Formato YYYY-MM-DD
+  console.log(fechaSeleccionada);
+
+asientos.forEach((asiento) => {
+    asiento.classList.remove("seleccionado");
+    actualizarAsientosSeleccionados();
+  });
+
+  // Haz una solicitud GET a la ruta /reservations/<date>
+  fetch(`/reservations/${fechaSeleccionada}`)
+    .then(response => response.json())
+    .then(reservas => {
+    // Por cada puesto reservado, agrega la clase 'reservado' al elemento correspondiente
+    reservas.forEach(puesto => {
+      let asiento = document.getElementById(`${puesto}`);
+      asiento.classList.add('reservado');
+    });
+  });
+});
+
+// Evento de clic en el asiento
+container.addEventListener("click", (e) => {
+
+  // Verifica si el elemento clickeado es un asiento y no está reservado
+  if (e.target.classList.contains("asiento") && !e.target.classList.contains("reservado")) {
+    
+    const asientoSeleccionado = e.target;
+
+    // Verifica si el asiento ya está seleccionado
+    const isSelected = asientoSeleccionado.classList.contains("seleccionado");
+    
+
+    // Obtiene todos los asientos seleccionados
+    const asientosSeleccionados = document.querySelectorAll(".row .asiento.seleccionado");
+
+    // Si el asiento no está seleccionado y ya hay un asiento seleccionado
+    if (!isSelected && asientosSeleccionados.length > 0) {
+      // Desmarca todos los asientos seleccionados
+      asientosSeleccionados.forEach((asiento) => {
+        asiento.classList.remove("seleccionado");
+      });
+    }
+
+    // Marca o desmarca el asiento seleccionado
+    asientoSeleccionado.classList.toggle("seleccionado");
+
+    // Si el asiento está seleccionado, asigna su id a idAsientoSeleccionado, de lo contrario, asigna null
+    idAsientoSeleccionado = asientoSeleccionado.classList.contains("seleccionado") ? asientoSeleccionado.id : null;
+    console.log(idAsientoSeleccionado);
+
+    // Actualiza la lista de asientos seleccionados
+    actualizarAsientosSeleccionados();
+  }
+});
+
+function actualizarAsientosSeleccionados() {
+  const asientosSeleccionados = document.querySelectorAll(".row .asiento.seleccionado");
+
+  // Si hay más de un asiento seleccionado, deselecciona todos excepto el último seleccionado
+  if (asientosSeleccionados.length > 1) {
+    asientosSeleccionados.forEach((asiento, index) => {
+      if (index < asientosSeleccionados.length - 1) {
+        asiento.classList.remove("seleccionado");
+      }
+    });
+  }
+
+  // Actualiza la lista de asientos seleccionados
+  const asientosSeleccionadosUpdated = document.querySelectorAll(".row .asiento.seleccionado");
+  const asientosIndex = [...asientosSeleccionadosUpdated].map((asiento) => [...asientos].indexOf(asiento));
+
+  localStorage.setItem("asientosSeleccionados", JSON.stringify(asientosIndex));
+}
+
+document.getElementById('reservar').addEventListener('click', function() {;
+  const puesto = idAsientoSeleccionado; 
+  const fecha_agendamiento = fechaSeleccionada;
+
+  console.log(puesto, fecha_agendamiento);
+
+  if (!puesto || !fecha_agendamiento) {
+    console.error('Faltan datos para la reserva');
+    return;
+  }
+
+  fetch('http://localhost:5000/reservations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      puesto: puesto,
+      fecha_agendamiento: fecha_agendamiento,
+    }),
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+});
+
